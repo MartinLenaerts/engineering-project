ce7a525ef1b9217cd57901214fd8d687b19656de
martin <martin.lenaerts@hotmail.fr>
2023-03-04 18:43:42 +0100

    add outgoing and incoming attributes

--- classes/BPMN/flow/event/end_event.py
+++ classes/BPMN/flow/event/end_event.py
@@ -5,7 +5,3 @@ from constant.event_type import EventType
 class EndEvent(Event):
     def __init__(self, xml_element):
         super().__init__(xml_element, EventType.END)
-        self.incoming_id = []
-        for incoming in xml_element:
-            self.incoming_id.append(incoming.text)
-        self.incoming_element = []
--- classes/BPMN/flow/flow_object.py
+++ classes/BPMN/flow/flow_object.py
@@ -1,6 +1,19 @@
 from classes.BPMN.bpmn_element import BPMNElement
+from constant.bpmn_xml_elements import BpmnXmlElement
 
 
 class FlowObject(BPMNElement):
     def __init__(self, xml_element):
         super().__init__(xml_element)
+
+        self.incoming_elements = []
+        self.outgoing_elements = []
+        self.incoming_ids = []
+        self.outgoing_ids = []
+
+        for xml_child in xml_element:
+            if xml_child.tag == BpmnXmlElement.OUTGOING.value:
+                self.incoming_ids.append(xml_child.text)
+
+            if xml_child.tag == BpmnXmlElement.INCOMING.value:
+                self.outgoing_ids.append(xml_child.text)
