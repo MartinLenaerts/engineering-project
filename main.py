999218da9bf0e7ba33efeb55c6181efc7703d0ac
martin <martin.lenaerts@hotmail.fr>
2023-03-22 18:29:25 +0100

    update args

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import logging
 import sys
 from utils.env import load_and_check_env
-from utils.utils_functions import get_args, parse_and_translate_from_file
+from utils.utils_functions import get_args, parse_and_translate
 
 if __name__ == '__main__':
     logs_file, petri_output_dir = load_and_check_env()
@@ -9,6 +9,9 @@ if __name__ == '__main__':
     logging.basicConfig(filename=logs_file, level=logging.DEBUG)
 
     logging.debug("\r\n\r\n################## New Execution ##################")
-    filename_arg, output_arg = get_args(sys.argv[1:])
+    filename_arg, output_arg, keep = get_args(sys.argv[1:])
 
-    parse_and_translate_from_file(filename_arg, output_arg, petri_output_dir)
+    if keep:
+        petri_output_dir = None
+
+    parse_and_translate(filename_arg, output_arg, petri_output_dir)
--- utils/utils_functions.py
+++ utils/utils_functions.py
@@ -23,50 +23,74 @@ def get_files_from_dir(dir_path):
     return res
 
 
-def parse_and_translate_from_file(filename_arg, output_type, output_dir):
-    dir_path = 'resources/bpmn_diagrams'
+def parse_and_translate_from_file(filename, output_type, output_dir):
+    file_name, _ = os.path.splitext(filename)
+    filename_without_ext, a = os.path.splitext(os.path.basename(file_name))
 
-    for path in get_files_from_dir(dir_path):
-        file_name, file_extension = os.path.splitext(path)
-        filename_without_ext, _ = os.path.splitext(os.path.basename(file_name))
-        if filename_arg is None or filename_without_ext == filename_arg:
-            try:
-                logging.debug("\r\n !!!!!! file : {} !!!!!! ".format(path))
+    try:
+        logging.debug("\r\n !!!!!! file : {} !!!!!! ".format(filename))
 
-                bpmn_diagram = parse_bpmn_file(path)
+        bpmn_diagram = parse_bpmn_file(filename)
 
-                logging.debug("{} file parsed".format(filename_without_ext))
-                petri_net = bpmn_to_petri(bpmn_diagram)
+        logging.debug("{} file parsed".format(filename_without_ext))
+        petri_net = bpmn_to_petri(bpmn_diagram)
 
-                logging.debug("{} petri net generated".format(filename_without_ext))
+        logging.debug("{} petri net generated".format(filename_without_ext))
 
-                if output_type == "txt":
-                    img_path = "{}.txt".format(file_name.replace(dir_path, output_dir))
-                    os.makedirs(os.path.dirname(img_path), exist_ok=True)
-                    petri_net_to_text(petri_net, img_path)
-                else:
-                    img_path = "{}.png".format(file_name.replace(dir_path, output_dir))
-                    os.makedirs(os.path.dirname(img_path), exist_ok=True)
-                    petri_net_to_img(petri_net, img_path)
+        files_created = []
+        if output_dir is not None:
+            filepath = file_name.replace(os.path.dirname(filename), output_dir)
+        else:
+            filepath = file_name
+        if output_type is None or (output_type == "txt"):
+            txt_path = "{}.txt".format(filepath)
+            os.makedirs(os.path.dirname(txt_path), exist_ok=True)
+            petri_net_to_text(petri_net, txt_path)
+            files_created.append(txt_path)
 
-                logging.info("✅ {} petri net save in png ({}) ✅".format(filename_without_ext, img_path))
-            except Exception as e:
-                logging.critical("‼️ Error in {} file, Message : '{}' ‼️".format(filename_without_ext, e))
-                logging.error(traceback.format_exc())
+        if output_type is None or (output_type == "png"):
+            img_path = "{}.png".format(filepath)
+            os.makedirs(os.path.dirname(img_path), exist_ok=True)
+            petri_net_to_img(petri_net, img_path)
+            files_created.append(img_path)
+
+        logging.info("✅ {} petri net save in png ({}) ✅".format(filename_without_ext, ', '.join(files_created)))
+    except Exception as e:
+        logging.critical("‼️ Error in {} file, Message : '{}' ‼️".format(filename_without_ext, e))
+        logging.error(traceback.format_exc())
+
+
+def parse_and_translate(filename_arg, output_type, output_dir):
+    default_dir_path = 'resources/bpmn_diagrams'
+
+    if filename_arg is None:
+        filename_arg = default_dir_path
+
+    if not os.path.exists(filename_arg) and os.path.exists("{}/{}.bpmn".format(default_dir_path, filename_arg)):
+        filename_arg = "{}/{}.bpmn".format(default_dir_path, filename_arg)
+
+    if os.path.isdir(filename_arg):
+        for path in get_files_from_dir(filename_arg):
+            parse_and_translate_from_file(path, output_type, output_dir)
+    else:
+        parse_and_translate_from_file(filename_arg, output_type, output_dir)
 
 
 def get_args(argv):
-    opts, args = getopt.getopt(argv, "hf:o:", ["filename=", "output="])
+    opts, args = getopt.getopt(argv, "hf:o:k", ["filename=", "output="])
     filename = None
-    output = "png"
+    output = None
+    keep = False
     for opt, arg in opts:
         if opt == '-h':
-            print('main.py -f <bpmn_diagrams file name> -o <output type (png or txt)')
+            print('main.py -f <bpmn_diagrams file name> -o <output type (png or txt)>')
             sys.exit()
         elif opt in ("-f", "--filename"):
             filename = arg
         elif opt in ("-o", "--output"):
             output = arg
-    logging.debug('Arguments ==>  filename: {}, output: {}'.format(filename, output))
+        elif opt in ("-k", "--keep"):
+            keep = True
+    logging.debug('Arguments ==>  filename: {}, output: {}, keep: {}'.format(filename, output, keep))
 
-    return filename, output
+    return filename, output, keep
