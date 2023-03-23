91446c07bb72f2ef27ecae453e43678b5a4bd5cc
martin <martin.lenaerts@hotmail.fr>
2023-03-23 15:10:32 +0100

    update transitions names

--- classes/PETRI/transition.py
+++ classes/PETRI/transition.py
@@ -4,15 +4,16 @@ from classes.PETRI.petri_element import PetriElement
 class Transition(PetriElement):
     def __init__(self, name, t_id, petri_net, color="grey"):
         super().__init__()
-        if name == "":
-            name = "None"
-        self.name = name
         self.id = t_id
+        if name == "":
+            name = None
+        else:
+            for t in petri_net.transitions:
+                if (t.name == name and name is not None) or (self.id == t.id):
+                    name = None
+        self.name = name
         self.color = color
         self.petri_net = petri_net
-        for t in petri_net.transitions:
-            if (t.name == name and name is not None) or (self.id == t.id):
-                raise Exception("❗ ( name : {}, id : {} ) transition already exist ! ❗️".format(self.name, self.id))
         self.petri_net.add_transition(self)
 
     def __str__(self):
